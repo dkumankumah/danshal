4fd655cd52648d8e003fb1b2eee8385628bb6832
D. Kaagman <dian.kaagman@hotmail.com>
2021-02-16 13:37:03 +0100

    Setup navigation to admin view

--- app/src/main/java/com/example/danshal/ui/admin/AdminFragment.kt
+++ app/src/main/java/com/example/danshal/ui/admin/AdminFragment.kt
@@ -1,4 +1,4 @@
-package com.example.danshal.ui.slideshow
+package com.example.danshal.ui.admin
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.danshal.R
 
-class SlideshowFragment : Fragment() {
+class AdminFragment : Fragment() {
 
-    private lateinit var slideshowViewModel: SlideshowViewModel
+    private lateinit var adminViewModel: AdminViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        slideshowViewModel =
-                ViewModelProvider(this).get(SlideshowViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
+        adminViewModel =
+                ViewModelProvider(this).get(AdminViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_admin, container, false)
         val textView: TextView = root.findViewById(R.id.text_slideshow)
-        slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
+        adminViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root

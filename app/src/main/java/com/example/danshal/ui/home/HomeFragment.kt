721f59992b1d2c232a6ebee9274694e0ac140806
Shaif <shaif.bhaggoe@gmail.com>
2021-02-20 14:59:45 +0100

    created adapter and event items for binding purposes

--- app/src/main/java/com/example/danshal/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/danshal/ui/home/HomeFragment.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.danshal.R
+import com.example.danshal.databinding.FragmentHomeBinding
+import com.example.danshal.models.Event
 
 class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+    private val events = arrayListOf<Event>()
+    private val homeAdapter = HomeAdapter(events)
 
     private lateinit var homeViewModel: HomeViewModel
 
@@ -26,6 +34,17 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
-        return root
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.rvEvents.layoutManager = GridLayoutManager(context, 1)
+        binding.rvEvents.adapter = homeAdapter
     }
 }

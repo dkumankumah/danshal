a795572f03579d81175566d3b4212f6a05bacd15
Shaif <shaif.bhaggoe@gmail.com>
2021-03-03 14:00:12 +0100

    events wont cut off ath the bottom

--- app/src/main/java/com/example/danshal/models/Event.kt
+++ app/src/main/java/com/example/danshal/models/Event.kt
@@ -10,14 +10,15 @@ data class Event (
     var title: String,
     var content: String,
     var date: Date,
+    var exclusive: Boolean,
     @DrawableRes var image: Int,
 ) {
     companion object {
         val EVENT_EXAMPLES = arrayOf(
-            Event(1, "Title event 1", "Content voor event 1", Date(2021, 3, 9), R.drawable.event1),
-            Event(2, "Title event 2", "Content voor event 2", Date(2021, 3, 21), R.drawable.event2),
-            Event(3, "Title event 4", "Content voor event 3", Date(2021, 3, 28), R.drawable.event3),
-            Event(4, "Title event 4", "Content voor event 4", Date(2021, 4, 8), R.drawable.event4),
+            Event(1, "Title event 1", "Content voor event 1", Date(2021, 3, 9), false, R.drawable.event1),
+            Event(2, "Title event 2", "Content voor event 2", Date(2021, 3, 21), true, R.drawable.event2),
+            Event(3, "Title event 4", "Content voor event 3", Date(2021, 3, 28), false, R.drawable.event3),
+            Event(4, "Title event 4", "Content voor event 4", Date(2021, 4, 8), true, R.drawable.event4),
         )
     }
 }
--- app/src/main/java/com/example/danshal/ui/home/HomeAdapter.kt
+++ app/src/main/java/com/example/danshal/ui/home/HomeAdapter.kt
@@ -3,6 +3,7 @@ package com.example.danshal.ui.home
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.danshal.R
 import com.example.danshal.databinding.ItemEventBinding

7a4c71d2dc60ad84d93ded7d51518dcf0a8a086a
Daniel Kumankumah <danielkumankumah@gmail.com>
2021-03-09 04:37:54 +0100

    data connection, write to database fixed

--- app/src/main/java/com/example/danshal/ui/models/User.kt
+++ app/src/main/java/com/example/danshal/ui/models/User.kt
@@ -1,9 +1,9 @@
 package com.example.danshal.ui.models
 
 data class User(val naam: String,
+                val adres: String,
                 val postcode: String,
                 val plaats: String,
-                val adres: String,
                 val email: String,
                 val userId: String,
                 val isAdmin: Int = 0 )

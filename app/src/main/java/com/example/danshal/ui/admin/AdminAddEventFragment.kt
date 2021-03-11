d5df138eb1806b5db7f648ac8cfbc84be3f9c32f
D. Kaagman <dian.kaagman@hotmail.com>
2021-03-11 16:54:00 +0100

    Adding event fragment first form

--- app/src/main/java/com/example/danshal/ui/admin/AdminAddEventFragment.kt
+++ app/src/main/java/com/example/danshal/ui/admin/AdminAddEventFragment.kt
@@ -1,32 +1,64 @@
 package com.example.danshal.ui.admin
 
+import android.app.DatePickerDialog
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.lifecycle.Observer
-import com.example.danshal.R
+import com.example.danshal.databinding.AdminAddEventFragmentBinding
+import java.util.*
 
 class AdminAddEventFragment : Fragment() {
 
     private lateinit var adminAddEventViewModel: AdminAddEventViewModel
 
+    private var _binding: AdminAddEventFragmentBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         adminAddEventViewModel =
             ViewModelProvider(this).get(AdminAddEventViewModel::class.java)
-        val root = inflater.inflate(R.layout.admin_add_event_fragment, container, false)
-        val textView: TextView = root.findViewById(R.id.text_admin_add_event)
-        adminAddEventViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+
+        _binding = AdminAddEventFragmentBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        addDatePicker()
+    }
+
+    private fun addDatePicker() {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        binding.btnAddDate.setOnClickListener {
+
+            context?.let {
+                val dpd = DatePickerDialog(it, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                    // Display Selected date in TextView
+                    binding.tvAddDate.text = "Datum: $dayOfMonth-$month-$year"
+                }, year, month, day)
+                dpd.show()
+            }
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
--- app/src/main/java/com/example/danshal/ui/admin/AdminAddEventViewModel.kt
+++ app/src/main/java/com/example/danshal/ui/admin/AdminAddEventViewModel.kt
@@ -1,12 +1,6 @@
 package com.example.danshal.ui.admin
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class AdminAddEventViewModel : ViewModel() {
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is admin add event Fragment"
-    }
-    val text: LiveData<String> = _text
 }

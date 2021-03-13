2ee922d46921fcd1a61851c8376351ab4435ca67
Shaif <shaif.bhaggoe@gmail.com>
2021-03-13 16:09:52 +0100

    Added filter function

--- app/src/main/java/com/example/danshal/ui/home/HomeAdapter.kt
+++ app/src/main/java/com/example/danshal/ui/home/HomeAdapter.kt
@@ -11,10 +11,11 @@ import com.example.danshal.databinding.ItemEventBinding
 import com.example.danshal.models.Event
 
 
-class HomeAdapter(private val events: List<Event>): RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
+class HomeAdapter(private val events: List<Event>) :
+    RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
 
     // Inner class for binding
-    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         // Binding to the item_event.xml
         val binding = ItemEventBinding.bind(itemView)
@@ -27,7 +28,7 @@ class HomeAdapter(private val events: List<Event>): RecyclerView.Adapter<HomeAda
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
-                LayoutInflater.from(parent.context).inflate(R.layout.item_event, parent, false)
+            LayoutInflater.from(parent.context).inflate(R.layout.item_event, parent, false)
         )
     }
 
--- app/src/main/java/com/example/danshal/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/danshal/ui/home/HomeFragment.kt
@@ -1,19 +1,17 @@
 package com.example.danshal.ui.home
 
+import android.content.Context
 import android.os.Bundle
 import android.view.*
-import android.widget.GridLayout
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.danshal.R
 import com.example.danshal.databinding.FragmentHomeBinding
 import com.example.danshal.models.Event
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
@@ -21,27 +19,24 @@ class HomeFragment : Fragment() {
     private val events = arrayListOf<Event>()
     private val homeAdapter = HomeAdapter(events)
     private val upEventAdapter = UpEventAdapter(events)
+    private var currentEventType: String? = null
 
     private lateinit var homeViewModel: HomeViewModel
 
     // Menu options:
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        currentEventType = getString(R.string.title_event)
         // let the app know that this fragment is expecting menu related callbacks
         setHasOptionsMenu(true)
     }
 
-    // Merge menu of this fragment with the appBar. Because of the inflater instantation
-    // a fragment menu has been created
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.main, menu)
-    }
-
     override fun onPrepareOptionsMenu(menu: Menu) {
-        super.onPrepareOptionsMenu(menu)
-        // Set teh visibility of the items in the menu
-        menu.findItem(R.id.action_settings).isVisible = true
+        val itemToHide = menu.findItem(R.id.action_settings)
+        itemToHide.isVisible = false
+
         menu.findItem(R.id.action_filter).isVisible = true
+        super.onPrepareOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -55,12 +50,12 @@ class HomeFragment : Fragment() {
     }
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
@@ -77,9 +72,10 @@ class HomeFragment : Fragment() {
 
     private fun initViews() {
         binding.rvEvents.layoutManager = GridLayoutManager(context, 1)
-        binding.rvEvents.adapter = homeAdapter
-//        binding.rvUpEvents.layoutManager = GridLayoutManager(context, 1)
-//        binding.rvUpEvents.adapter = upEventAdapter
+
+        binding.rvEvents.adapter = if (currentEventType == getString(R.string.title_event)) homeAdapter else upEventAdapter
+
+        println(currentEventType)
 
         for (i in Event.EVENT_EXAMPLES.indices) {
             events.add(Event.EVENT_EXAMPLES[i])
@@ -90,13 +86,32 @@ class HomeFragment : Fragment() {
     }
 
     private fun openFilterWindow() {
-        Toast.makeText(context, "test", Toast.LENGTH_SHORT).show()
-        val dialog = EventFilterDialogFragment()
-        dialog.showsDialog
+        val dialogItems =
+            arrayOf(getString(R.string.title_event), getString(R.string.title_up_event))
+        val checkedItem = dialogItems.indexOf(currentEventType)
+
+        context?.let {
+            MaterialAlertDialogBuilder(it)
+                .setTitle(resources.getString(R.string.title_filter))
+                .setNeutralButton(resources.getString(R.string.action_cancel_filter)) { dialog, which ->
+                    // Respond to neutral button press
+                }
+                .setPositiveButton(resources.getString(R.string.action_filter)) { dialog, which ->
+                    // Respond to positive button press
+                    initViews()
+                }
+                // Single-choice items (initialized with checked item)
+                .setSingleChoiceItems(dialogItems, checkedItem) { dialog, which ->
+                    // Respond to item chosen
+                    println(which)
+                    currentEventType = dialogItems[which]
+                }
+                .show()
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null;
+        _binding = null
     }
 }

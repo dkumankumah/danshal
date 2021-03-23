d6ba76b7a56440ae19c9873460d6a760d05ad63d
D. Kaagman <dian.kaagman@hotmail.com>
2021-03-23 13:13:11 +0100

    Add image to Event

--- app/src/main/java/com/example/danshal/models/Event.kt
+++ app/src/main/java/com/example/danshal/models/Event.kt
@@ -1,9 +1,6 @@
 package com.example.danshal.models
 
-import androidx.annotation.DrawableRes
-import com.example.danshal.R
 import com.google.firebase.Timestamp
-import java.time.LocalDate
 import java.util.*
 
 data class Event (
@@ -12,15 +9,15 @@ data class Event (
     var address: Address,
     var date: Date,
     var exclusive: Boolean,
-    @DrawableRes var image: Int,
+    var imageUrl: String? = null,
     val timestamp: Timestamp? = Timestamp.now()
 ) {
     companion object {
         val EVENT_EXAMPLES = arrayOf(
-            Event("Title event 1", "Content voor event 1", Address(1, null,"1091 GR","Wibautstraat", "Amsterdam"),  Date(2021, 3, 5), false, R.drawable.event1),
-            Event("Title event 2", "Content voor event 2", Address(21, null,"1091 GR","Wibautstraat", "Amsterdam"),  Date(2021, 3, 21), true, R.drawable.event2),
-            Event("Title event 4", "Content voor event 3", Address(33, null,"1091 GR","Wibautstraat", "Amsterdam"),  Date(2021, 3, 28), false, R.drawable.event3),
-            Event("Title event 4", "Content voor event 4", Address(17, null,"1091 GR","Wibautstraat", "Amsterdam"),  Date(2021, 4, 8), true, R.drawable.event4),
+            Event("Title event 1", "Content voor event 1", Address(1, null,"1091 GR","Wibautstraat", "Amsterdam"),  Date(2021, 3, 5), false, "https://firebasestorage.googleapis.com/v0/b/danshal-c7e70.appspot.com/o/content_images%2Fevent-dTgrSiJxoX5imtc5ZlQe.jpg?alt=media&token=f751965d-bf53-425b-ae1b-11681f81dcd5"),
+            Event("Title event 2", "Content voor event 2", Address(21, null,"1091 GR","Wibautstraat", "Amsterdam"),  Date(2021, 3, 21), true, "https://firebasestorage.googleapis.com/v0/b/danshal-c7e70.appspot.com/o/content_images%2Fevent-dTgrSiJxoX5imtc5ZlQe.jpg?alt=media&token=f751965d-bf53-425b-ae1b-11681f81dcd5"),
+            Event("Title event 4", "Content voor event 3", Address(33, null,"1091 GR","Wibautstraat", "Amsterdam"),  Date(2021, 3, 28), false),
+            Event("Title event 4", "Content voor event 4", Address(17, null,"1091 GR","Wibautstraat", "Amsterdam"),  Date(2021, 4, 8), true),
         )
     }
 }
--- app/src/main/java/com/example/danshal/ui/home/HomeAdapter.kt
+++ app/src/main/java/com/example/danshal/ui/home/HomeAdapter.kt
@@ -1,17 +1,19 @@
 package com.example.danshal.ui.home
 
+import android.content.Context
 import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.danshal.R
 import com.example.danshal.databinding.ItemEventBinding
 import com.example.danshal.models.Event
 
-
 class HomeAdapter(private val events: List<Event>): RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
+    private lateinit var context: Context
 
     // Inner class for binding
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
@@ -21,14 +23,15 @@ class HomeAdapter(private val events: List<Event>): RecyclerView.Adapter<HomeAda
 
         fun databind(event: Event) {
             binding.tvEventTitle.text = event.title
-            binding.ivEventImage.setImageResource(event.image)
+            Glide.with(context).load(event.imageUrl).into(binding.ivEventImage)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(
-                LayoutInflater.from(parent.context).inflate(R.layout.item_event, parent, false)
-        )
+        val vh = LayoutInflater.from(parent.context).inflate(R.layout.item_event, parent, false)
+        context = vh.context
+
+        return ViewHolder(vh)
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
